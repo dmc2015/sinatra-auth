ab6a6f17a61d8774ae4a8002a8939475fd727d70
Don McLamb <mclamb.donald@gmail.com>
2016-04-06 17:13:39 -0400

    added a check to see the level of scope that is being provided

--- server.rb
+++ server.rb
@@ -9,3 +9,33 @@ CLIENT_SECRET = ENV['GH_CLIENT_SECRET']
 get '/' do
   erb :index, :locals => {:client_id => CLIENT_ID}
 end
+
+
+get '/callback' do
+  #temp github code
+  session_code = request.env['rack.request.quest_hash']['code']
+
+  #post temp session_code to github
+
+  result = RestClient.post('https://github.com/login/oauth/access_token',
+    {:client => CLIENT_ID,
+    :client_secret => CLIENT_SECRET,
+    :code => session_code},
+    :accept => :json
+  )
+
+
+  #take the token and scope from the result
+  access_token = JSON.parse(result)['access_token']
+
+  #checks to see the level of scope that you have
+  scopes = JSON.parse(result)['scope'].split(',')
+  has_user_email_scope = scopes.include? 'user:email'
+
+end
+
+
+get '/revoke' do
+  erb: index, :locals => {:client_id => CLIENT_ID}
+
+end
